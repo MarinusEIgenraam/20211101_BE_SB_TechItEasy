03ba67739d71fce1b7273367077458daf960dd5c
Whitedragon1987 <vanoostenjohan@hotmail.com>
2021-11-04 15:19:46 +0100

    answer first assigment

---
+++ src/main/java/nl/novi/techiteasyassigment/Controllers/TelevisionController.java
@@ -0,0 +1,44 @@
+package nl.novi.techiteasyassigment.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class TelevisionController {
+
+    @GetMapping("/televisions")
+    public ResponseEntity<Object> getAllTelevisions() {
+
+        return ResponseEntity.ok();
+
+    }
+
+    @GetMapping("/televisions/{id}")
+    public ResponseEntity<Object> getTelevision(@PathVariable("id") Long id) {
+
+        return ResponseEntity.ok();
+
+    }
+
+    @PostMapping("/televisions")
+    public ResponseEntity<Object> addTelevision(@RequestBody String television) {
+
+        return ResponseEntity.created();
+
+    }
+
+    @DeleteMapping("/televisions/{id}")
+    public ResponseEntity<Object> deleteTelevision(@PathVariable("id") Long id) {
+
+        return ResponseEntity.noContent();
+
+    }
+
+    @PutMapping("televisions/{id}")
+    public ResponseEntity<Object> updateTelevision(@PathVariable("id") Long id, @RequestBody String television) {
+
+        return ResponseEntity.noContent();
+
+    }
+}
