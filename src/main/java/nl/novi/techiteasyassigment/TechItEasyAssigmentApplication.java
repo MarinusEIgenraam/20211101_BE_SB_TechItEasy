799018f44f145fc57e9c13b52207b5b31f3d24e6
Whitedragon1987 <vanoostenjohan@hotmail.com>
2021-11-04 14:46:31 +0100

    first commit

---
+++ src/main/java/nl/novi/techiteasyassigment/TechItEasyAssigmentApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasyassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasyAssigmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechItEasyAssigmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/techiteasyassigment/TechItEasyAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasyassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyAssigmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
