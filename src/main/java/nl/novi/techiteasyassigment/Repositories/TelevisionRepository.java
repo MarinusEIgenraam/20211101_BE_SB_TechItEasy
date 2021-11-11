09f52e91879db5157c955306d25477282a9ad783
MarinusEIgenraam <rinuseigenraam@gmail.com>
2021-11-11 22:03:00 +0100

    opdracht 2

---
+++ src/main/java/nl/novi/techiteasyassigment/Repositories/TelevisionRepository.java
@@ -0,0 +1,8 @@
+package nl.novi.techiteasyassigment.Repositories;
+
+import nl.novi.techiteasyassigment.Models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class TelevisionRepository extends JpaRepository<Television, Long> {
+
+}
